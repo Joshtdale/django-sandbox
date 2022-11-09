699577357c49d6aa889430bc9e730979c534107e
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-11-09 19:56:35 +0000

    working

--- library/books/admin.py
+++ library/books/admin.py
@@ -4,5 +4,5 @@ from .models import Book
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
-    fields = ('title', 'category', 'description', 'published_year', 'price')
+    fields = ('title', 'category', 'description', 'published_year', 'price', 'cuisine')
 admin.site.register(Book, BookAdmin)
--- library/books/models.py
+++ library/books/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from datetime import date
 
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+class Cuisine(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
 class Book(models.Model):
     title = models.CharField(max_length=300)
     published_year = models.IntegerField(null=True, blank=True)
     # category = models.CharField(default='Lunch', max_length=30)
-    category = models.IntegerField(null=True, blank=True)
-    cuisine = models.IntegerField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, default=None)
+    cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE, null=True, blank=True)
     description = models.CharField(default='Its probably edible', max_length=300)
     price = models.IntegerField(null=True, blank=True)
     # author = models.CharField(max_length=30)
@@ -14,5 +26,3 @@ class Book(models.Model):
     def __str__(self):
         return self.title + " " + str(self.published_year)
 
-class Category(models.Model):
-    
